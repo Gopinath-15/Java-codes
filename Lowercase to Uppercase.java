650b79a52ca779af3f3a9a685616d7b7ceec5644
Gopinath <147173554+Gopinath-15@users.noreply.github.com>
2025-07-11 22:55:53 +0530

    Create Lowercase to Uppercase.java

---
+++ Lowercase to Uppercase.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        n = n.toUpperCase();
+        System.out.println(n);
+        sc.close();
+    }
+}
