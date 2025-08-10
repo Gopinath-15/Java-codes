79a5c24c365d3ce30f25aeec7d3329c857602f46
Gopinath <147173554+Gopinath-15@users.noreply.github.com>
2025-08-10 21:28:25 +0530

    Create Star pattern.java

---
+++ Star pattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+ class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        
+        for (int i = 1; i <= n; i++) {
+        
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+        
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
